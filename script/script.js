634f7c6d326998ed39a9e1b8995a772510adee13
dags <dags-@users.noreply.github.com>
2016-06-13 19:08:04 +0100

    Update script.js

--- script/script.js
+++ script/script.js
@@ -2,6 +2,6 @@
     var link = document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
-    link.href = 'https://raw.githubusercontent.com/ArdaCraft/ardacraft.github.io/master/ardacraft/style/images/favicon.png';
+    link.href = 'https://ardacraft.github.io/ardacraft/style/images/favicon.png';
     document.getElementsByTagName('head')[0].appendChild(link);
 }());
