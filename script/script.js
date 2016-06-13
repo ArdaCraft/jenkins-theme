eab6d30c68741034190f992194a2b7d3ee0a099d
dags <dags-@users.noreply.github.com>
2016-06-13 09:53:25 +0100

    Create script.js

---
+++ script/script.js
@@ -0,0 +1,7 @@
+(function() {
+    var link = document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://raw.githubusercontent.com/ArdaCraft/ardacraft.github.io/master/ardacraft/style/images/favicon.png';
+    document.getElementsByTagName('head')[0].appendChild(link);
+}());
