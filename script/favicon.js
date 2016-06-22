fdcb5649288a969c15f1568a86b85c173fa66913
dags <dags-@users.noreply.github.com>
2016-06-22 09:18:16 +0100

    Update and rename script.js to favicon.js

--- script/favicon.js
+++ script/favicon.js
@@ -1,7 +1,7 @@
-(function() {
+document.observe("dom:loaded", function() {
     var link = document.createElement('link');
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
     link.href = 'https://ardacraft.github.io/ardacraft/style/images/favicon.png';
     document.getElementsByTagName('head')[0].appendChild(link);
-}());
+};);
